ea637614eb91cc2b27471b5c80c5f04bcdccf650
goskan93 <natalia.goska@gmail.com>
2023-03-05 18:07:54 +0100

    organize tsconfig

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import isPerfectNumber from "./perfect_number";
 import isArmstrongNumber from "./amstrong_number";
 
+console.log("works")
+console.log(isPerfectNumber(6))
 export default {
     isPerfectNumber,
     isArmstrongNumber
