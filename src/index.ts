e6667c5a0e8b474d4b4b73fcf8f6b3ebf7cce002
goskan93 <natalia.goska@gmail.com>
2023-03-05 18:39:54 +0100

    szlif

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import isPerfectNumber from "./perfect_number";
 import isArmstrongNumber from "./amstrong_number";
 
-console.log("works")
-console.log(isPerfectNumber(6))
 export default {
     isPerfectNumber,
     isArmstrongNumber
