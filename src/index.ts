c030cf5e3ff760c37fa229727d0b31db8360857c
goskan93 <natalia.goska@gmail.com>
2023-03-05 15:09:03 +0100

    poporawki

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-console.log("works!!!!")
-//import isPerfectNumber from "./perfect_number"
-import isArmstrongNumber from "./amstrong_number"
-//console.log(isPerfectNumber(6))
-console.log(isArmstrongNumber(92727))
+import isPerfectNumber from "./perfect_number";
+import isArmstrongNumber from "./amstrong_number";
+
+export default {
+    isPerfectNumber,
+    isArmstrongNumber
+}
